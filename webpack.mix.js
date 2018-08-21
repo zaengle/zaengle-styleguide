43d7893f454a95f0317d4711b12f942450a9291e
Philip Zaengle <philip@zaengle.com>
2018-08-21 11:12:53 -0400

    adds build process
    Signed-off-by: Philip Zaengle <philip@zaengle.com>

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+let mix = require('laravel-mix');
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
+
+mix
+    .js('src/js/styleguide.js', 'dist/')
+    .sass('src/scss/styleguide.scss', 'dist/')
+    .tailwind()
+    .copyDirectory('dist', 'example')
+    .browserSync({
+      proxy: 'http://styleguide.test',
+      files: ['example/**']
+    });
+
+if (mix.inProduction()) {
+  mix
+    .purgeCss({
+      enabled: true,
+      globs: [
+        'example/**/*.php',
+        'dist/**/*.js'
+      ],
+      extensions: ['html', 'js', 'php']
+    });
+}
